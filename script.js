348e7f77654350c4721d0417a94c9a91c9733b82
nathan.tundrea <natanael.tundrea@emanuel.ro>
2024-03-21 12:40:18 +0200

    bb8

--- script.js
+++ script.js
@@ -2,5 +2,21 @@ function setup(){
  createCanvas(800,600)
 }
 function draw(){
-    
+    nathanFace()
+}
+
+function nathanFace(){
+    background("white")
+    fill("orange");
+    circle(400, 300, 50);
+    fill("black");
+    circle(400, 290, 13);
+    fill("black");
+    circle(410, 300, 10);
+    fill("white");
+    circle(400, 360, 100);
+    fill("orange");
+    circle(400, 360, 75);
+    fill("white");
+    circle(400, 360, 60);
 }
