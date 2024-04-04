84aa57000c5de7c466b2fe34404924318df98f14
Giorgiana <giorgiana.dinu@emanuel.ro>
2024-04-04 12:38:50 +0300

    ray

--- smGiorgi.js
+++ smGiorgi.js
@@ -1,45 +1,51 @@
 let smGiorgi = {
   x: 200,
   y: 300,
+  ray: 45,
   directionX: 3,
   directionY: 3,
-  speed: 30
+  speedX: 5,
+  speedY: 5
 }
 
 function checkLimitsSMGiorgi() {
-    if (smGiorgi.x >= tabWith || smGiorgi.x <= 0) smGiorgi.directionX *= -1;
+    if (smGiorgi.x >= tabWith - smGiorgi.ray || smGiorgi.x <= 0 + smGiorgi.ray) {
+      smGiorgi.directionX *= -1;
+    }
 
-    smGiorgi.x += smGiorgi.directionX * smGiorgi.speed;
+    smGiorgi.x += smGiorgi.directionX * smGiorgi.speedX;
 
-    if (smGiorgi.y >= tabWith || smGiorgi.y <= 0) smGiorgi.directionY *= -1;
+    if (smGiorgi.y >= tabWith - smGiorgi.ray || smGiorgi.y <= 0 + smGiorgi.ray) {
+      smGiorgi.directionY *= -1;
+    }
 
-    smGiorgi.x += smGiorgi.directionX * smGiorgi.speed;
-  }
+    smGiorgi.y += smGiorgi.directionY * smGiorgi.speedY;
+    }
   function smilyFaceGiorgi(x, y) {
     //Body
     // fill("white");
     fill("beige");
     stroke("black");
-    circle(x, y, 50);
+    circle(x, y, smGiorgi.ray * 2);
     //EYSE
     fill("black")
     stroke("black");
-    ellipse(x - 10, y - 10, 10, 5);
-    ellipse(x + 10, y - 10, 10, 5);
+    ellipse(x - (smGiorgi.ray * 2) / 5, y - (smGiorgi.ray * 2) / 5, (smGiorgi.ray * 2) / 5, (smGiorgi.ray * 2) / 10);
+    ellipse(x + (smGiorgi.ray * 2) / 5, y - (smGiorgi.ray * 2) / 5, (smGiorgi.ray * 2) / 5, (smGiorgi.ray * 2) / 10);
     //Eye pupils
     stroke("light green");
     fill("lightgreen");
-    circle(x - 10, y - 10, 4);
-    circle(x + 10, y - 10, 4);
+    circle(x - (smGiorgi.ray * 2) / 5, y - (smGiorgi.ray * 2) / 5, (smGiorgi.ray * 2) / 12.5);
+    circle(x + (smGiorgi.ray * 2) / 5, y - (smGiorgi.ray * 2) / 5, (smGiorgi.ray * 2) / 12.5);
   
     //Mounth
     fill("black")
     stroke("red");
-    arc(x, y + 10, 17, 10, 0, Math.PI);
+    arc(x, y + (smGiorgi.ray * 2) / 5, (smGiorgi.ray * 2) / 2.94, (smGiorgi.ray * 2) / 5, 0, Math.PI);
   
     //Cheeks
     stroke("red");
     fill("red");
-    circle(x + 20, y + 5, 9);
-    circle(x - 20, y + 5, 9);
+    circle(x + (smGiorgi.ray * 2) / 2.5, y + (smGiorgi.ray * 2) / 10, (smGiorgi.ray * 2) / 5.5);
+    circle(x - (smGiorgi.ray * 2) / 2.5, y + (smGiorgi.ray * 2) / 10, (smGiorgi.ray * 2) / 5.5);
   }
