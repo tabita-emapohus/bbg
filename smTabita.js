9bed53433da0327e0c2100122f1eb7bcb2555112
Tabita Pohus <emanuela.pohus@emanuel.ro>
2024-04-04 12:39:47 +0300

    dimensiuni egale

--- smTabita.js
+++ smTabita.js
@@ -1,49 +1,53 @@
 let smTabita = {
     x:400,
     y:100,
+    ray:35,
     directionX: 3,
     directionY: 3,
-    speed:20
+    speedX:3,
+    speedY:3
 }
 
 
 function checkLimitsSMTabita() {
-    if (smTabita.x >= tabWith || smTabita.x <= 0) 
+    if (smTabita.x >= tabWith - smTabita.ray || smTabita.x <= 0 + smTabita.ray) {
     smTabita.directionX *= -1;
+    }
 
-    smTabita.x += smTabita.directionX * smTabita.speed;
+    smTabita.x += smTabita.directionX * smTabita.speedX;
 
-    if(smTabita.y >= tabWith || smTabita.y <= 0) 
+    if(smTabita.y >= tabHeight - smTabita.ray || smTabita.y <= 0 + smTabita.ray) {
     smTabita.directionY *= -1;
+    }
 
-    smTabita.x += smTabita.directionX * smTabita.speed;
+    smTabita.y += smTabita.directionY * smTabita.speedY;
   }
   function smilyFaceTabita(x, y) {
     //Body
     // fill("white");
     fill("gray");
     stroke("black");
-    circle(x, y, 50);
+    circle(x, y, smTabita.ray * 2);
     //EYSE
     fill("black")
     stroke("black");
-    ellipse(x - 10, y - 10, 10, 5);
-    ellipse(x + 10, y - 10, 10, 5);
+    ellipse(x - (smTabita.ray * 2)/5, y - (smTabita.ray * 2)/5, (smTabita.ray * 2)/5, (smTabita.ray * 2)/10);
+    ellipse(x + (smTabita.ray * 2)/5, y - (smTabita.ray * 2)/5, (smTabita.ray * 2)/5, (smTabita.ray * 2)/10);
     //Eye pupils
     stroke("light green");
     fill("lightgreen");
-    circle(x - 10, y - 10, 4);
-    circle(x + 10, y - 10, 4);
+    circle(x - (smTabita.ray * 2)/5, y - (smTabita.ray * 2)/5, (smTabita.ray * 2)/12.5);
+    circle(x + (smTabita.ray * 2)/5, y - (smTabita.ray * 2)/5, (smTabita.ray * 2)/12.5);
   
     //Mounth
     fill("black")
     stroke("red");
-    arc(x, y + 10, 17, 10, 0, Math.PI);
+    arc(x, y + (smTabita.ray * 2)/5, (smTabita.ray * 2)/2.95, (smTabita.ray * 2)/5, 0, Math.PI);
   
     //Cheeks
     stroke("red");
     fill("red");
-    circle(x + 20, y + 5, 9);
-    circle(x - 20, y + 5, 9);
+    circle(x + (smTabita.ray * 2)/2.5, y + (smTabita.ray * 2)/10, (smTabita.ray * 2)/5.5);
+    circle(x - (smTabita.ray * 2)/2.5, y + smTabita.ray * 2/10, (smTabita.ray * 2)/5.5);
   }
   
