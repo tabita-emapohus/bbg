610a55e3b303dd6c07ca68109d822b670f821efb
nathan.tundrea <natanael.tundrea@emanuel.ro>
2024-04-11 09:40:57 +0300

    nathan paddle

---
+++ pdNathan.js
@@ -0,0 +1,12 @@
+let pdNathan = {
+    x: 100,
+    y: 200,
+    color: "red",
+    width: 50,
+    height: 25
+}
+
+function createPdNathan(){
+    fill(pdNathan.color);
+    rect(pdNathan.x, pdNathan.y, pdNathan.width, pdNathan.height);
+}
